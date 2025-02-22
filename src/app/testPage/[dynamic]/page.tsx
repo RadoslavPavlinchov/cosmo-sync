9d949fee5fcca4fa80687171662bf4b46c59a4cb
Radoslav <radoslav.pavlinchov@gmail.com>
2025-02-22 14:56:52 +0200

    chore: add initial project setup

---
+++ src/app/testPage/[dynamic]/page.tsx
@@ -0,0 +1,9 @@
+export default async function DynamicParams({
+    params,
+}: {
+    params: Promise<{ dynamic: string }>
+}) {
+    const dynamicParam = (await params).dynamic
+
+    return <h1>Dynamic data from url {dynamicParam}</h1>
+}
